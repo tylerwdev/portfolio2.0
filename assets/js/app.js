f3beee0acee744b0c22af4bf781de2d2cd3995a9
Tyler Winters <tylerwintersdev@gmail.com>
2022-03-31 20:23:23 -0700

    removed carousel Jquery code and changed github link on contact

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,11 @@
 // *START* This is where JS goes for making css component styles work
 
-$(document).ready(function(){
-  $('.sidenav').sidenav();
+$(document).ready(function () {
+  $(".sidenav").sidenav();
 });
 
-$(document).ready(function(){
-  $('.modal').modal();
+$(document).ready(function () {
+  $(".modal").modal();
 });
 
-$(document).ready(function(){
-  $('.carousel').carousel();
-});
 // *END* This is where JS goes for making css component styles work
