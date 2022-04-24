5f0dc6d9e3ab0e2e2d0c54d14d41a86336b63664
Tyler Winters <tylerwintersdev@gmail.com>
2022-04-23 17:13:48 -0700

    made changes to portfolio working on contact form

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,4 +8,8 @@ $(document).ready(function () {
   $(".modal").modal();
 });
 
+$(document).ready(function () {
+  $("input#input_text, textarea#textarea2").characterCounter();
+});
+
 // *END* This is where JS goes for making css component styles work
